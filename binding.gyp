9a2bacfa6651f1b7469df9f26b02365788689bfb
stormwin <ketejadam@hotmail.com>
2021-04-02 14:28:47 +0200

    Fix for node v12

--- binding.gyp
+++ binding.gyp
@@ -6,6 +6,9 @@
         'src/LocationManager.mm',
         'src/CLLocationBindings.mm'
       ],
+      "include_dirs" : [
+        "<!(node -e \"require('nan')\")"
+      ],
       'link_settings': {
         'libraries': [
           'CoreLocation.framework'
--- src/CLLocationBindings.mm
+++ src/CLLocationBindings.mm
@@ -1,130 +1,135 @@
 #include <math.h>
-#include <node.h>
-#include <v8.h>
+#include <nan.h>
 
 #import "LocationManager.h"
 
-using namespace v8;
-using namespace node;
+void getCurrentPosition(const Nan::FunctionCallbackInfo<v8::Value>& args) {
+	v8::Isolate* isolate = args.GetIsolate();
 
-void getCurrentPosition(const FunctionCallbackInfo<Value>& args) {
-  Isolate* isolate = args.GetIsolate();
-  HandleScope scope(isolate);
+	LocationManager* locationManager = [[LocationManager alloc] init];
 
-  LocationManager* locationManager = [[LocationManager alloc] init];
+	v8::Local<v8::Context> context = v8::Context::New(isolate);
 
-  if (args.Length() == 1) {
-    if (args[0]->IsObject()) {
-      Local<Object> options = args[0]->ToObject();
+	if (args.Length() == 1) {
+		if (args[0]->IsObject()) {
+			v8::Local<v8::Object> options = args[0]->ToObject(context).ToLocalChecked();
 
-      Local<String> maximumAgeKey = String::NewFromUtf8(isolate, "maximumAge");
-      if (options->Has(maximumAgeKey)) {
-        // Anything less than 100ms doesn't make any sense
-        locationManager.maximumAge = fmax(
-          100, options->Get(maximumAgeKey)->NumberValue()
-        );
-        locationManager.maximumAge /= 1000.0;
-      }
+			v8::Local<v8::String> maximumAgeKey = v8::String::NewFromUtf8(isolate, "maximumAge").ToLocalChecked();
+			if (options->Has(context, maximumAgeKey).FromMaybe(false)) {
+				// Anything less than 100ms doesn't make any sense
+				locationManager.maximumAge = fmax(
+					100,
+					Nan::To<double>(Nan::Get(options, maximumAgeKey).ToLocalChecked()).FromJust()
+				);
+				locationManager.maximumAge /= 1000.0;
+			}
 
-      Local<String> enableHighAccuracyKey = String::NewFromUtf8(
-        isolate, "enableHighAccuracy"
-      );
-      if (options->Has(enableHighAccuracyKey)) {
-        locationManager.enableHighAccuracy = options->Get(
-          enableHighAccuracyKey
-        )->BooleanValue();
-      }
+			v8::Local<v8::String> enableHighAccuracyKey = v8::String::NewFromUtf8(
+				isolate, 
+				"enableHighAccuracy"
+			).ToLocalChecked();
+			if (options->Has(context, enableHighAccuracyKey).FromMaybe(false)) {
+				locationManager.enableHighAccuracy = Nan::To<bool>(Nan::Get(options, enableHighAccuracyKey).ToLocalChecked()).FromJust();
+			}
 
-      Local<String> timeout = String::NewFromUtf8(
-        isolate, "timeout"
-      );
-      if (options->Has(timeout)) {
-        locationManager.timeout = options->Get(timeout)->NumberValue();
-      }
+			v8::Local<v8::String> timeout = v8::String::NewFromUtf8(isolate, "timeout").ToLocalChecked();
+			if (options->Has(context, timeout).FromMaybe(false)) {
+				locationManager.timeout = Nan::To<uint32_t>(Nan::Get(options, timeout).ToLocalChecked()).FromJust();
+			}
+		}
+	}
 
-    }
-  }
+	if (![CLLocationManager locationServicesEnabled]) {
+		isolate->ThrowException(
+			Exception::TypeError(
+				v8::String::NewFromUtf8(isolate, "CLocationErrorNoLocationService").ToLocalChecked()
+			)
+		);
+		return;
+	}
 
-  if (![CLLocationManager locationServicesEnabled]) {
-    isolate->ThrowException(
-      Exception::TypeError(
-        String::NewFromUtf8(isolate, "CLocationErrorNoLocationService")
-      )
-    );
-    return;
-  }
+	CLLocation* location = [locationManager getCurrentLocation];
 
-  CLLocation* location = [locationManager getCurrentLocation];
+	if ([locationManager hasFailed]) {
+		switch (locationManager.errorCode) {
+			case kCLErrorDenied:
+				isolate->ThrowException(
+					Exception::TypeError(
+						v8::String::NewFromUtf8(
+							isolate,
+							"CLocationErrorLocationServiceDenied"
+						).ToLocalChecked()
+					)
+				);
+				return;
+			case kCLErrorGeocodeCanceled:
+				isolate->ThrowException(
+					Exception::TypeError(
+						v8::String::NewFromUtf8(isolate, "CLocationErrorGeocodeCanceled").ToLocalChecked()
+					)
+				);
+				return;
+			case kCLErrorLocationUnknown:
+				isolate->ThrowException(
+					Exception::TypeError(
+						v8::String::NewFromUtf8(isolate, "CLocationErrorLocationUnknown").ToLocalChecked()
+					)
+				);
+				return;
+			default:
+				isolate->ThrowException(
+					Exception::TypeError(
+						v8::String::NewFromUtf8(isolate, "CLocationErrorLookupFailed").ToLocalChecked()
+					)
+				);
+				return;
+		}
+	}
 
-  if ([locationManager hasFailed]) {
-    switch (locationManager.errorCode) {
-      case kCLErrorDenied:
-        isolate->ThrowException(
-            Exception::TypeError(
-              String::NewFromUtf8(
-                isolate,
-                "CLocationErrorLocationServiceDenied"
-              )
-            )
-        );
-        return;
-      case kCLErrorGeocodeCanceled:
-        isolate->ThrowException(
-            Exception::TypeError(
-              String::NewFromUtf8(isolate, "CLocationErrorGeocodeCanceled")
-            )
-        );
-        return;
-      case kCLErrorLocationUnknown:
-        isolate->ThrowException(
-            Exception::TypeError(
-              String::NewFromUtf8(isolate, "CLocationErrorLocationUnknown")
-            )
-        );
-        return;
-      default:
-        isolate->ThrowException(
-            Exception::TypeError(
-              String::NewFromUtf8(isolate, "CLocationErrorLookupFailed")
-            )
-        );
-        return;
-      }
-  }
+	v8::Local<v8::Object> obj = v8::Object::New(isolate);
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "latitude").ToLocalChecked(),
+		v8::Number::New(isolate, location.coordinate.latitude)
+	);
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "longitude").ToLocalChecked(),
+		v8::Number::New(isolate, location.coordinate.longitude)
+	);
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "altitude").ToLocalChecked(),
+		v8::Number::New(isolate, location.altitude)
+	);
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "horizontalAccuracy").ToLocalChecked(),
+		v8::Number::New(isolate, location.horizontalAccuracy)
+	);
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "verticalAccuracy").ToLocalChecked(),
+		Number::New(isolate, location.verticalAccuracy)
+	);
 
-  Local<Object> obj = Object::New(isolate);
-  obj->Set(
-    String::NewFromUtf8(isolate, "latitude"),
-    Number::New(isolate, location.coordinate.latitude)
-  );
-  obj->Set(
-    String::NewFromUtf8(isolate, "longitude"),
-    Number::New(isolate, location.coordinate.longitude)
-  );
-  obj->Set(
-    String::NewFromUtf8(isolate, "altitude"),
-    Number::New(isolate, location.altitude)
-  );
-  obj->Set(
-    String::NewFromUtf8(isolate, "horizontalAccuracy"),
-    Number::New(isolate, location.horizontalAccuracy)
-  );
-  obj->Set(
-    String::NewFromUtf8(isolate, "verticalAccuracy"),
-    Number::New(isolate, location.verticalAccuracy)
-  );
+	NSTimeInterval seconds = [location.timestamp timeIntervalSince1970];
+	Nan::Set(
+		obj,
+		v8::String::NewFromUtf8(isolate, "timestamp").ToLocalChecked(),
+		Number::New(isolate, (NSInteger)ceil(seconds * 1000))
+	);
 
-  NSTimeInterval seconds = [location.timestamp timeIntervalSince1970];
-  obj->Set(
-    String::NewFromUtf8(isolate, "timestamp"),
-    Number::New(isolate, (NSInteger)ceil(seconds * 1000))
-  );
-
-  args.GetReturnValue().Set(obj);
+	args.GetReturnValue().Set(obj);
 }
 
-void Initialise(Handle<Object> exports) {
-  NODE_SET_METHOD(exports, "getCurrentPosition", getCurrentPosition);
+void Initialise(v8::Local<v8::Object> exports) {
+	v8::Local<v8::Context> context = exports->CreationContext();
+	exports->Set(
+		context,
+		Nan::New("getCurrentPosition").ToLocalChecked(),
+		Nan::New<v8::FunctionTemplate>(getCurrentPosition)->GetFunction(context).ToLocalChecked()
+	);
 }
 
 NODE_MODULE(macos_clocation_wrapper, Initialise)
